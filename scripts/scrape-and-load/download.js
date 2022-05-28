c4ff78c0d4f254b7eb8644b51d102dd2333fa2e4
Jeremia Kimelman <jbkimelman@gmail.com>
2022-05-27 17:09:39 -0700

    Try again?

--- scripts/scrape-and-load/download.js
+++ scripts/scrape-and-load/download.js
@@ -9,16 +9,10 @@ const __dirname = path.dirname(__filename)
 const downloadPath = path.join(__dirname, "../../tmp")
 
 export default async function downloadNetfile({ agencyId, year }) {
-    const goToUrl = async(u) => {
-        await page.goto(u)
-        return page
-    }
     const downloadPageUrl = `https://public.netfile.com/pub2/Default.aspx?aid=${agencyId}`
-    const browser = await puppeteer.launch({ headless: true, timeout: 60000 })
+    const browser = await puppeteer.launch({ headless: false, timeout: 60000 })
     const page = await browser.newPage()
 
-    console.log('a', agencyId)
-    await goToUrl(downloadPageUrl)
     console.log('b', agencyId)
     await page.goto(downloadPageUrl)
 
@@ -46,7 +40,7 @@ export default async function downloadNetfile({ agencyId, year }) {
     // downloading, let's just wait 5 seconds and see if
     // that works for now. shrug
     // const fileName = `${aid}-${year}.zip`
-    await page.waitForTimeout(15000)
+    await page.waitForTimeout(7000)
     console.log('h', agencyId)
     await browser.close()
 }
