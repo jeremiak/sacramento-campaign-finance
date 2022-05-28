58579a85b0eac4992e335944aa463b4f55792c38
Jeremia Kimelman <jbkimelman@gmail.com>
2022-05-27 17:32:55 -0700

    Yikes

--- scripts/scrape-and-load/download.js
+++ scripts/scrape-and-load/download.js
@@ -13,40 +13,48 @@ export default async function downloadNetfile({ agencyId, year }) {
     const browser = await puppeteer.launch({ headless: true, timeout: 60000 })
     const page = await browser.newPage()
 
+    async function goto() {
+        await page.goto(downloadPageUrl, { timeout: 60000 })
+    }
+
     try {
         console.log('b', agencyId)
-        await page.goto(downloadPageUrl, { timeout: 60000 })
-
-        if (page.url() !== downloadPageUrl) {
-            console.log('bb')
-            await page.goto(downloadPageUrl, { timeout: 60000 })
-        }
-
-        console.log('c', agencyId)
-
-        await page._client.send("Page.setDownloadBehavior", {
-            behavior: "allow",
-            downloadPath,
-        })
-
-        console.log('d', agencyId)
-
-        await page.waitForSelector("#ctl00_phBody_DateSelect")
-        console.log('e', agencyId)
-        await page.select("#ctl00_phBody_DateSelect", year)
-        console.log('f', agencyId)
-        await page.click("#ctl00_phBody_GetExcel")
-        console.log('g', agencyId)
-
-        // instead of trying to figure out if the file is done
-        // downloading, let's just wait 5 seconds and see if
-        // that works for now. shrug
-        // const fileName = `${aid}-${year}.zip`
-        await page.waitForTimeout(7000)
-        console.log('h', agencyId)
-        await browser.close()
-        return
+        await goto()
     } catch (e) {
-        console.error(`ERR`, e)
+        console.error('up here', e)
+        await goto()
     }
+
+    if (page.url() !== downloadPageUrl) {
+        console.log('bb')
+        await page.goto(downloadPageUrl, { timeout: 60000 })
+    }
+
+    console.log('c', agencyId)
+
+    await page._client.send("Page.setDownloadBehavior", {
+        behavior: "allow",
+        downloadPath,
+    })
+
+    console.log('d', agencyId)
+
+    await page.waitForSelector("#ctl00_phBody_DateSelect")
+    console.log('e', agencyId)
+    await page.select("#ctl00_phBody_DateSelect", year)
+    console.log('f', agencyId)
+    await page.click("#ctl00_phBody_GetExcel")
+    console.log('g', agencyId)
+
+    // instead of trying to figure out if the file is done
+    // downloading, let's just wait 5 seconds and see if
+    // that works for now. shrug
+    // const fileName = `${aid}-${year}.zip`
+    await page.waitForTimeout(7000)
+    console.log('h', agencyId)
+    await browser.close()
+    return
+} catch (e) {
+    console.error(`ERR`, e)
+}
 }
