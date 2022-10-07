5eb008f40589af3c7d9a83c2f5c534c06fb8b0c6
Jeremia Kimelman <jbkimelman@gmail.com>
2022-10-06 21:21:36 -0700

    Style IE to match direct contributions

--- src/lib/IE.svelte
+++ src/lib/IE.svelte
@@ -1,39 +1,57 @@
 <script>
+  import _ from 'lodash'
   export let name = ''
   export let data = []
+
+  $: total = _.sumBy(data, d => d.total)
 </script>
 
 <div>
+  <h3>Independent expenditures</h3>
   {#if data.length > 0}
-    <div class="e">
-      <h2>Independent expenditures</h2>
-      <ul>
-        {#each data as ie}
-          <li>
-            <p>
-              <a
-                href="https://cal-access.sos.ca.gov/Campaign/Committees/Detail.aspx?id={ie.spenderId}&view=received"
-                >{ie.spenderName}</a
-              >
-              has spent
-              <span class="monospace">${ie.total.toLocaleString("en-US")}</span>
-              to {#if ie.position === "S"}support{:else}oppose{/if}
-              {name}.
-            </p>
-          </li>
-        {/each}
-      </ul>
-    </div>
+    <p>${Math.floor(total).toLocaleString("en-US")} from {data.length} spender{#if data.length !== 1}s{/if}{#if data.length !== 0}:{/if}</p>
+    <ul>
+      {#each data as ie}
+        <li>
+          <p>
+            <a
+              href="https://cal-access.sos.ca.gov/Campaign/Committees/Detail.aspx?id={ie.spenderId}&view=received"
+              >{ie.spenderName}</a
+            >
+            has spent
+            <span class="monospace">${ie.total.toLocaleString("en-US")}</span>
+            to {#if ie.position === "S"}support{:else}oppose{/if}
+            {name}.
+          </p>
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No independent spending to support or oppose</p>
   {/if}
 </div>
 
 <style lang="scss">
   ul {
+    font-size: 1rem;
     list-style-type: none;
     padding-left: 0;
   }
 
-  h2 {
-    font-size: 1.2rem;
+  li {
+    padding: .5rem;
+  }
+
+  li:nth-child(even) {
+    background-color: #ebebeb;
+  }
+
+  p {
+    font-size: 1rem;
+    margin: 0;
+  }
+
+  h3 {
+    font-size: 1rem;
   }
 </style>
