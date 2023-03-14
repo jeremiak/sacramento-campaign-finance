390d1a98f43c12e94525f4de70f947aece32fd1e
Jeremia Kimelman <jbkimelman@gmail.com>
2023-03-13 22:16:07 -0700

    Include footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,5 +6,15 @@
 	<slot />
 </main>
 
+<footer>
+	Made by <a href="https://www.jeremiak.com">Jeremia</a> in 2023
+</footer>
+
 <style lang="scss">
+	footer {
+		font-size: 14px;
+		line-height: 18px;
+		margin-bottom: 1rem;
+		text-align: center;
+	}
 </style>
